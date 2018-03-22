8d757c13cf0eba2b354df2375c3f3d5669f6e274
Richard <richard.armitage@gov.bc.ca>
2018-03-22 11:52:14 -0700

    chnaged names to lower case

--- client/src/components/dropdown/SearchResults.vue
+++ client/src/components/dropdown/SearchResults.vue
@@ -26,11 +26,11 @@
 
 <script>
 /* eslint-disable */
-  import compnameview from '@/components/dropdown/search/compName.vue';
-  import compinfoview from '@/components/dropdown/search/compType.vue';
-  import compmatches from '@/components/dropdown/search/nameMatches.vue';
-  import matchissues from '@/components/dropdown/search/IssueInfo.vue';
-  import examinationmenu from '@/components/dropdown/search/examinationMenu.vue';
+  import compnameview from '@/components/dropdown/search/compname.vue';
+  import compinfoview from '@/components/dropdown/search/comptype.vue';
+  import compmatches from '@/components/dropdown/search/namematches.vue';
+  import matchissues from '@/components/dropdown/search/issueinfo.vue';
+  import examinationmenu from '@/components/dropdown/search/examinationmenu.vue';
 
   export default {
     name: "searchResults",
