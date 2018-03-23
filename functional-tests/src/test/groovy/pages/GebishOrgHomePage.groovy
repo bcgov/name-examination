c325b62826166b192559bb92e988cb4ce053b88d
Trent <LTRENT@BCGOV>
2018-03-23 12:34:38 -0700

    BDD Samples
    Spock spec and GEB samples.

---
+++ functional-tests/src/test/groovy/pages/GebishOrgHomePage.groovy
@@ -0,0 +1,10 @@
+import geb.Page
+
+class GebishOrgHomePage extends Page {
+
+    static at = { title == "Geb - Very Groovy Browser Automation" }
+
+    static content = {
+        manualsMenu { module(ManualsMenuModule) }
+    }
+}
