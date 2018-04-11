951d19976ef2e28fb0c1e6fe7031c5d4f4183016
Richard <richard.armitage@gov.bc.ca>
2018-04-11 15:17:43 -0700

    Fixed Search results ??

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -58,6 +58,7 @@ let router = new Router({
 
 router.beforeResolve((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
+    console.log('Auth check')
     // this route requires auth,
     // if not Authenticated, redirect to login page.
     if (store.state.idToken === null) {
@@ -66,6 +67,7 @@ router.beforeResolve((to, from, next) => {
         path: '/'
       })
     } else {
+      console.log('Authorized')
       next()
     }
   } else {
