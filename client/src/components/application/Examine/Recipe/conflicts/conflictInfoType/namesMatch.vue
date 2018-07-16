401119b154b1ea91a4130f5c938d5c1e43dfec3a
Kial Jinnah <kialj876@gmail.com>
2018-07-16 10:07:10 -0700

    #647 error from null values fixed

--- client/src/components/application/Examine/Recipe/conflicts/conflictInfoType/namesMatch.vue
+++ client/src/components/application/Examine/Recipe/conflicts/conflictInfoType/namesMatch.vue
@@ -41,81 +41,97 @@
     name: 'namesMatch',
     computed: {
       clientFirstName() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.clientFirstName == undefined) return '';
           return this.namesConflictInfo.applicants.clientFirstName
 
       },
       clientLastName() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.clientLastName == undefined) return '';
           return this.namesConflictInfo.applicants.clientLastName
       },
       firstName() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.firstName == undefined) return '';
           return this.namesConflictInfo.applicants.firstName
 
       },
       middleName() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.middleName == undefined) return '';
           return this.namesConflictInfo.applicants.middleName
 
       },
       lastName() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.lastName == undefined) return '';
           return this.namesConflictInfo.applicants.lastName
 
       },
       addressLine1() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.addrLine1 == undefined) return '';
           return this.namesConflictInfo.applicants.addrLine1
 
       },
       addressLine2() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.addrLine2 == undefined) return '';
           return this.namesConflictInfo.applicants.addrLine2
 
       },
       addressLine3() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.addrLine3 == undefined) return '';
           return this.namesConflictInfo.applicants.addrLine3
 
       },
       city() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.city == undefined) return '';
           return this.namesConflictInfo.applicants.city
 
       },
       province() {
-          if (this.namesConflictInfo == undefined) return '';
-          return this.namesConflictInfo.applicants.statePprovinceCd
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.stateProvinceCd == undefined) return '';
+          return this.namesConflictInfo.applicants.stateProvinceCd
 
       },
       country() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.countryTypeCd == undefined) return '';
           return this.namesConflictInfo.applicants.countryTypeCd
 
       },
       postalCode() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.postalCd == undefined) return '';
         return this.namesConflictInfo.applicants.postalCd
 
       },
       contactName() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.contact == undefined) return '';
           return this.namesConflictInfo.applicants.contact
 
       },
       phone() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.phoneNumber == undefined) return '';
           return this.namesConflictInfo.applicants.phoneNumber
 
       },
       conEmail() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.emailAddress == undefined) return '';
           return this.namesConflictInfo.applicants.emailAddress
 
       },
       fax() {
-          if (this.namesConflictInfo == undefined) return '';
+          if (this.namesConflictInfo == undefined || this.namesConflictInfo.applicants == undefined ||
+            this.namesConflictInfo.applicants.faxNumber == undefined) return '';
           return this.namesConflictInfo.applicants.faxNumber
 
       },
