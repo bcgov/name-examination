8b32f26bc7532973c61ca6bbdc442e816fc76932
Katie McGoff <katie@silverbirchsolutions.com>
2018-07-16 22:20:52 -0700

    Added loading spinner for longer-running ajax requests.

--- client/src/axios-auth.js
+++ client/src/axios-auth.js
@@ -8,6 +8,8 @@ const instance = axios.create({
 let isAlreadyFetchingAccessToken = false
 let subscribers = []
 
+var activeRequestsCounter = 0;
+
 function onAccessTokenFetched(access_token) {
   subscribers = subscribers.filter(callback => callback(access_token))
 }
@@ -16,12 +18,56 @@ function addSubscriber(callback) {
   subscribers.push(callback)
 }
 
-axios.interceptors.response.use(function (response) {
+instance.interceptors.request.use(function (config) {
+  /*
+  1. Increase the counter of how many active requests there are in progress.
+  2. Create a timer to show the spinner after 1 second; the delay is so we don't flash in up for
+      very fast requests.
+  3. Save the timer to this request so it can be passed to the response/error handling and
+      cancelled. Otherwise we get orphaned timers that do not get cancelled when the response is
+      returned, and the loading spinner never goes away.
+   */
+  activeRequestsCounter++;
+  var loadingTimer = setTimeout(function () {
+    $('#loading-overlay').css('display', 'flex');
+  }, 1000);
+  config.loadingTimer = loadingTimer;
+
+  return config;
+});
+
+
+
+instance.interceptors.response.use(function (response) {
+  /*
+  1. Cancel the timer associated with this request.
+  2. Decrease the list of requests running timers.
+  3. If there are no more requests that might still be running, hide the overlay.
+   */
+  clearTimeout(response.config.loadingTimer);
+  activeRequestsCounter--;
+  if (activeRequestsCounter <= 0) {
+    activeRequestsCounter = 0;
+    $('#loading-overlay').css('display', 'none');
+  }
+
   return response
 }, function (error) {
+  /*
+  1. cancel the timer associated with this request
+  2. decrease the list of requests running timers
+  3. if there are no more requests that might still be running, hide the overlay
+   */
+  clearTimeout(error.config.loadingTimer);
+  activeRequestsCounter--;
+  if (activeRequestsCounter <= 0) {
+    activeRequestsCounter = 0;
+    $('#loading-overlay').css('display', 'none');
+  }
+
+  /* This code was never executed before implementing above, so commented out since untested.
   const { config, response: { status } } = error
   const originalRequest = config
-
   if (status === 401) {
     if (!isAlreadyFetchingAccessToken) {
       isAlreadyFetchingAccessToken = true
@@ -39,6 +85,7 @@ axios.interceptors.response.use(function (response) {
     })
     return retryOriginalRequest
   }
+  */
   return Promise.reject(error)
 })
 
