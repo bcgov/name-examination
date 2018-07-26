60aea4419561a53e5a443b019fa5cdd539419540
Katie McGoff <katie@silverbirchsolutions.com>
2018-07-26 02:46:53 -0700

    Added request type edit rules and validation using vuelidate. Simple 'required' validation, plus accuracy check on Previous NR and Corp Num. Additional Info built from template based on request type.

--- client/static/js/utils.js
+++ client/static/js/utils.js
@@ -23,6 +23,51 @@ function readJFile(configUrl, cBack) {
     return null;
 }
 
+function readCsv(csvFile, callback) {
+  console.log('got to readCsv()');
+
+  if (csvFile) {
+    var req = new XMLHttpRequest();
+    req.open('GET', csvFile, true);
+
+    req.onreadystatechange = function () {
+      if (req.readyState == 4) {
+        if (req.status == 200) {
+
+          var allTextLines = req.responseText.split(/\r\n|\n/);
+
+          // create what will be a list of objects
+          var lines = [];
+
+          // loop through all lines and parse data into object
+          for (var i=1; i < allTextLines.length; i++) {
+            var data = allTextLines[i].split(',');
+
+            var obj = {
+              'request_type': data[0],
+              'nwpta_required': data[2]=='TRUE'?true:false,
+              'jurisdiction_required': data[3]=='TRUE'?true:false,
+              'prev_nr_required': data[4]=='TRUE'?true:false,
+              'corp_num_required': data[5]=='TRUE'?true:false,
+              'additional_info_template': data[6],
+            }
+
+            lines.push(obj);
+          }
+          callback(lines);
+        } else {
+          callback(null);
+        }
+      }
+    };
+
+    req.send();
+
+  } else {
+    callback(null);
+  }
+  return null;
+}
 
 function findArrValue(needle) {
   // assumes the haystack is an object with attribute "value" to match to
@@ -35,6 +80,12 @@ function findArrText(needle) {
   // assumes the haystack is an object with attribute "text" to match to
   return function (haystack) {
     return needle == haystack.text;
+  }
+}
+
+function findArrValueByAttr(needle, attribute) {
+  return function (haystack) {
+    return needle == eval('haystack.' + attribute);
   }
 }
 
