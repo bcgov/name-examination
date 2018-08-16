b1bec49f63829a9cae77c931ba370ddbf2371ad9
Kial Jinnah <kialj876@gmail.com>
2018-08-16 10:12:02 -0700

    history update -- tab moved to bottom, highlight A = green, alert banner change

--- client/src/components/application/Examine/RecipeMenu.vue
+++ client/src/components/application/Examine/RecipeMenu.vue
@@ -1,20 +1,13 @@
-/* eslint-disable */
+<!--eslint-disable-->
 <template>
   <div class="col-2">
 
     <div class="nav flex-column nav-pills recipe-steps" id="v-pills-tab" role="tablist"
          aria-orientation="vertical">
 
-      <div id="History1">
-        <i id="History2"></i></div>
-      <a id="history-tab" class="nav-link active" data-toggle="pill" href="#"
-         @click="clickRecipeCard('History')">History</a>
-      <div class="arrow-right"></div>
-
-
       <div id="Conflict1">
         <i id="Conflict2"></i></div>
-      <a id="conflicts-tab" class="nav-link" data-toggle="pill" href="#"
+      <a id="conflicts-tab" class="nav-link active" data-toggle="pill" href="#"
          @click="clickRecipeCard('Conflicts')">
         Conflicts
       </a>
@@ -32,6 +25,12 @@
          @click="clickRecipeCard('Trademarks')">Trademarks</a>
       <div class="arrow-right"></div>
 
+      <div id="History1">
+        <i id="History2"></i></div>
+      <a id="history-tab" class="nav-link" data-toggle="pill" href="#"
+         @click="clickRecipeCard('History')">History</a>
+      <div class="arrow-right"></div>
+
 
     </div>
   </div>
@@ -43,7 +42,7 @@
 export default {
   name: 'matches',
   mounted() {
-    this.currentRecipeCard = "History"
+    this.currentRecipeCard = "Conflicts"
     this.setConflicts();
     this.setConditions();
     this.setTrademarks();
