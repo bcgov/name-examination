4d553bcf7fc28af2ee6fe005a4482497e66b7757
Richard <richard.armitage@gov.bc.ca>
2018-08-29 07:51:32 -0700

    State Header

---
+++ client/src/components/application/sections/StateHeader.vue
@@ -0,0 +1,25 @@
+<!--eslint-disable-->
+<template>
+  <span>
+    <p class="state-text">{{currentState}}</p>
+  </span>
+</template>
+
+<script>
+/* eslint-disable */
+  export default {
+    name: "stateHeader",
+    computed: {
+      currentState() {
+        return this.$store.getters.currentState
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .state-text {
+    text-align: center;
+    font-size: 1.5em;
+  }
+</style>
