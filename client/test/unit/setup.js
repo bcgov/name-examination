51f21737659dded99b25418e293dd4941c90030a
Eric Mignot <eric.mignot@gmail.com>
2018-10-10 16:22:30 -0700

    exploring CompName

--- client/test/unit/setup.js
+++ client/test/unit/setup.js
@@ -1,3 +1,6 @@
 import Vue from 'vue';
 
 Vue.config.productionTip = false;
+
+import $ from 'jquery';
+global.$ = global.jQuery = $;
---
+++ client/test/unit/specs/CompName.spec.js
@@ -0,0 +1,45 @@
+import Vue from 'vue';
+Vue.use(require('vue-shortkey'))
+import CompName from '@/components/application/Examine/CompName';
+import store from '@/store'
+import axios from '@/axios-auth.js';
+import sinon from 'sinon';
+
+describe('CompName.vue', () => {
+
+    let instance;
+    beforeEach(() => {
+        const Constructor = Vue.extend(CompName);
+        instance = new Constructor({store:store});
+        instance.$store.state.myKeycloak = {}
+    })
+
+    describe('Initialization', ()=>{
+        let sandbox;
+        let vm;
+        beforeEach((done) => {
+            sandbox = sinon.createSandbox()
+            sandbox.stub(axios, 'get').withArgs('/api/v1/requests/queues/@me/oldest', sinon.match.any).returns(
+                new Promise((resolve) => resolve({ data: { nameRequest:'NR 1234'} }))
+            )
+            instance.$store.state.currentState = 'INPROGRESS'
+            instance.$store.state.examiner = 'Joe'
+            instance.$store.state.userId = instance.$store.state.examiner
+            vm = instance.$mount();
+            setTimeout(()=>{
+                done();
+            }, 100)
+        })
+        afterEach(() => {
+            sandbox.restore()
+        })
+
+        it('shares your oldest NR', () => {
+            expect(vm.$store.getters.nrNumber).toEqual('NR 1234');
+        })
+
+        it('lets you quick-approve your assigned NR', () => {
+            expect(vm.$el.querySelector('#examine-quick-approve-button').textContent).toEqual('Quick Approve')
+        })
+    })
+});
