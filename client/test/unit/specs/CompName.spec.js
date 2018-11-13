139ae2aeb9e3a002572679913880e32a5cd5ba9b
perkinss <perkins.susan@gmail.com>
2018-11-13 10:52:07 -0800

    Improve the compname unit test. Get rid of errors on the setFocus method

--- client/test/unit/specs/CompName.spec.js
+++ client/test/unit/specs/CompName.spec.js
@@ -12,20 +12,22 @@ describe('CompName.vue', () => {
     beforeEach(() => {
         const Constructor = Vue.extend(CompName);
         instance = new Constructor({store:store});
-        instance.$store.state.myKeycloak = {}
-    })
+        instance.$store.state.myKeycloak = {};
+        instance.setFocus = () => {};
+    });
 
     describe('Initialization', ()=>{
         let sandbox;
         let vm;
         beforeEach((done) => {
-            sandbox = sinon.createSandbox()
+            sandbox = sinon.createSandbox();
             sandbox.stub(axios, 'get').withArgs('/api/v1/requests/queues/@me/oldest', sinon.match.any).returns(
                 new Promise((resolve) => resolve({ data: { nameRequest:'NR 1234'} }))
-            )
-            instance.$store.state.currentState = 'INPROGRESS'
-            instance.$store.state.examiner = 'Joe'
-            instance.$store.state.userId = instance.$store.state.examiner
+            );
+            instance.$store.state.currentState = 'INPROGRESS';
+            instance.$store.state.examiner = 'Joe';
+            instance.$store.state.userId = instance.$store.state.examiner;
+
             instance.$store.state.compInfo.compNames = {
                 compName1:
                   {choice: 1,
@@ -34,36 +36,38 @@ describe('CompName.vue', () => {
                     decision_text: "A Foreign Entity That Is Registering In British Columbia As An Extraprovincial Company And Adopting An Assumed Name Must Provide The Registrar With A Covering Letter Attaching An Undertaking To Carry On Business Under The Assumed Name.  Sample Working For The Undertaking Can Be Found On Page 34 Of The Information For Registration Of An "},
                 compName2: {choice: 2, name: "Good Name", state: 'NE'},
                 compName3: {choice: 3, name: null, state: 'NE'}
-            }
+            };
+
             vm = instance.$mount();
             setTimeout(()=>{
                 done();
             }, 100)
-        })
+        });
         afterEach(() => {
             sandbox.restore()
-        })
+        });
 
         it('shares your oldest NR', () => {
             expect(vm.$store.getters.nrNumber).toEqual('NR 1234');
-        })
+        });
 
         it('lets you quick-approve your assigned NR', () => {
             expect(vm.$el.querySelector('#examine-quick-approve-button').textContent).toEqual('Quick Approve')
-        })
+        });
         it('displays small decision text while in progress', () => {
               console.log(vm.$store.getters.compName1);
-              expect(vm.$store.getters.compName1.name).toEqual("Bad Name")
-              expect(vm.$el.querySelector('#decision-text1').className).toEqual('decision-text')
+              expect(vm.$store.getters.compName1.name).toEqual("Bad Name");
+              expect(vm.$el.querySelector('.completed')).toBeNull()
         })
-      })
+      });
 
       describe('Display full decision text after completion', ()=>{
         let sandbox;
         let vm;
+
         beforeEach((done) => {
-            sandbox = sinon.createSandbox()
-            instance.$store.state.currentState = 'APPROVED'
+            sandbox = sinon.createSandbox();
+            instance.$store.state.currentState = 'APPROVED';
             instance.$store.state.compInfo.compNames = {
               compName1:
                 {choice: 1,
@@ -72,18 +76,20 @@ describe('CompName.vue', () => {
                   decision_text: "A Foreign Entity That Is Registering In British Columbia As An Extraprovincial Company And Adopting An Assumed Name Must Provide The Registrar With A Covering Letter Attaching An Undertaking To Carry On Business Under The Assumed Name.  Sample Working For The Undertaking Can Be Found On Page 34 Of The Information For Registration Of An "},
               compName2: {choice: 2, name: "Good Name", state: 'ACCEPTED'},
               compName3: {choice: 3, name: null, state: 'NE'}
-            }
+            };
             vm = instance.$mount();
             setTimeout(()=>{
                 done();
             }, 100)
-        })
+        });
+
         afterEach(() => {
             sandbox.restore()
-        })
+        });
 
         it('displays full decision text once completed', () => {
-            expect(vm.$el.querySelector('#decision-text1').className).toEqual('decision-text completed')
+            expect(vm.$el.querySelectorAll('.completed')).not.toBeNull();
+            expect(vm.$el.querySelectorAll('.completed').length).toBe(3);
         })
     })
 
