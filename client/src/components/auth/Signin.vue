aa376b42d98a0af6cc2620eae8fdf4f4f82f9339
perkinss <perkins.susan@gmail.com>
2019-01-10 11:08:50 -0800

    remove unused comment

--- client/src/components/auth/Signin.vue
+++ client/src/components/auth/Signin.vue
@@ -36,7 +36,7 @@
             sessionStorage.setItem('KEYCLOAK_TOKEN', keycloak.token);
             sessionStorage.setItem('KEYCLOAK_REFRESH', keycloak.refreshToken);
             sessionStorage.setItem('KEYCLOAK_EXPIRES', keycloak.tokenParsed.exp * 1000);
-            // **** DO NOT COMMIT THIS: **** ////
+
             let roles =  keycloak.realmAccess.roles.filter(role => ALLOWED_ROLES.includes(role));
             sessionStorage.setItem('USER_ROLES', roles);
 
