7ad0ea8aec18f11f39a1ff135be67422a5cc9b60
Cameron-freshworks <cameron@freshworks.io>
2021-07-23 08:43:31 -0700

    Split requests mapping

---
+++ client/src/enums/entity-type-code.js
@@ -0,0 +1,9 @@
+/**
+ * Enum for Entity type codes.
+ * Used for mapping conversions to support namex ui's split entityTypes.
+ * */
+const EntityTypeCode = {
+  GP: 'GP', // General Partnership
+}
+
+export default Object.freeze(EntityTypeCode)
