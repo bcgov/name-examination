204394c867bfb7478485d2529546481ad6c57ca6
eve-git <evedeng@NE021325.idir.BCGOV>
2022-06-15 11:07:58 -0700

    replace vaule to corpNum in corpNum validation

--- client/src/components/application/Examine/RequestInfoHeader.vue
+++ client/src/components/application/Examine/RequestInfoHeader.vue
@@ -236,7 +236,7 @@
                   <v-text-field class="name-choice-input"
                                       v-model="corpNum"
                                       autocomplete="off"
-                                      @input="$v.corpNum.$touch()">
+                                      @blur="$v.corpNum.$touch()">
                     <template v-slot:append-outer>
                       <spinner style="transform: scale(.4); position: relative; top: -95px"
                                className="corp-num-spinner hidden" />
@@ -999,6 +999,7 @@
 
       // validate corp # - not required, but if entered it must be validated
       if (this.corp_num_required && !this.is_closed) {
+        this.corpNum = corpNum.replace(/^BC+/i, '')
         validations.corpNum = {
           isValidCorpNum(value) {
 
