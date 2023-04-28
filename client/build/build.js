760e3d076570677509b59e90bf12297c1b82a068
rarmitag <Richard.Armitage@gov.bc.ca>
2023-04-28 09:25:24 -0700

    fixed config files

--- client/build/build.js
+++ client/build/build.js
@@ -1,24 +1,32 @@
 'use strict'
 require('./check-versions')()
 
-process.env.NODE_ENV = 'production'
-
 const ora = require('ora')
 const rm = require('rimraf')
 const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
-const webpackConfig = require('./webpack.prod.conf')
+
+// const webpackConfig = require('./webpack.dev.conf') 
+const webpackConfig = require('./webpack.prod.conf') 
+
 
 const spinner = ora('building for production...')
+
 spinner.start()
 
+console.log(chalk.red('  Start building.\n'))
+console.log("Root " + config.build.assetsRoot)
+console.log("Sub " + config.build.assetsSubDirectory)
+
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
-  if (err) throw err
+
+if (err) throw err
   webpack(webpackConfig, (err, stats) => {
-    spinner.stop()
+    spinner.stop()   
     if (err) throw err
+
     process.stdout.write(stats.toString({
       colors: true,
       modules: false,
@@ -31,7 +39,7 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       console.log(chalk.red('  Build failed with errors.\n'))
       process.exit(1)
     }
-
+  
     console.log(chalk.cyan('  Build complete.\n'))
     console.log(chalk.yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
