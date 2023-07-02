cdad1834167ed642bf5349f1eb578ae52f1764d9
Madhavkapoor28 <madhav.kapoor@gov.bc.ca>
2023-07-02 16:56:17 -0700

    updated LD

--- plugins/initLaunchDarkly.ts
+++ plugins/initLaunchDarkly.ts
@@ -1,7 +1,22 @@
-import { initialize, LDClient, LDOptions } from 'launchdarkly-js-client-sdk'
+import { initialize, LDClient, LDFlagSet, LDOptions, LDUser } from 'launchdarkly-js-client-sdk'
 
 // get rid of "element implicitly has an 'any' type..."
 declare const window: any
+
+/**
+ * Default flag values when LD is not available.
+ * NB: Uses "business-edit" project (per LD client id in config).
+ */
+const defaultFlagSet: LDFlagSet = {
+  'banner-text': '', // by default, there is no banner text
+  'enable-digital-credentials': false,
+  'sentry-enable': false, // by default, no sentry logs
+  'show-alert-phone-numbers-firm': false,
+  'supported-business-summary-entities': [],
+  'supported-correction-entities': [],
+  'supported-dissolution-entities': [],
+  'supported-consent-continuation-out-entities': []
+}
 
 /**
  * The Launch Darkly client instance.
@@ -11,11 +26,11 @@ let ldClient: LDClient
 /**
  * An async method that initializes the Launch Darkly client.
  */
-export default async function initLdClient (): Promise<LDClient> {
+async function InitLdClient (): Promise<void> {
   const envKey: string = window['ldClientId']
 
   if (envKey) {
-    const user = {
+    const user: LDUser = {
       // since we have no user data yet, use a shared key temporarily
       key: 'anonymous'
     }
@@ -32,12 +47,46 @@ export default async function initLdClient (): Promise<LDClient> {
 
     try {
       await ldClient.waitForInitialization()
-    } catch (e) {
+    } catch {
       // shut down client -- `variation()` will return undefined values
       await ldClient.close()
       // NB: LD logs its own errors
     }
   }
+}
 
-  return ldClient
+/**
+ * An async method that updates the Launch Darkly user properties.
+ * @param key a unique string identifying a user
+ * @param email the user's email address
+ * @param firstName the user's first name
+ * @param lastName the user's last name
+ * @param custom optional object of additional attributes associated with the user
+ */
+async function UpdateLdUser (
+  key: string, email: string, firstName: string, lastName: string, custom: any = null
+): Promise<void> {
+  if (ldClient) {
+    const user: LDUser = { key, email, firstName, lastName, custom }
+    try {
+      await ldClient.identify(user)
+    } catch {
+      // NB: LD logs its own errors
+    }
+  }
+}
+
+/**
+ * A method that gets the value of the specified feature flag.
+ * @param name the name of the feature flag
+ * @returns the flag value/variation, or undefined if the flag is not found
+ */
+function GetFeatureFlag (name: string): any {
+  return ldClient ? ldClient.variation(name) : defaultFlagSet[name]
+}
+
+export default {
+  InitLdClient,
+  UpdateLdUser,
+  GetFeatureFlag
 }
