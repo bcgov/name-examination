8aef9638169e19df09be260a4c9c133a240f572e
Madhavkapoor28 <madhav.kapoor@gov.bc.ca>
2023-07-25 16:18:26 -0700

    small UI fix

--- components/StdHeader.vue
+++ components/StdHeader.vue
@@ -35,13 +35,9 @@
           <label
             for="allowWordClassificationModal"
             class="sr-only"
-          >NR Number Lookup
-          </label>
+          >NR Number Lookup</label>
           <div class="relative w-full">
-            <div
-              class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
-            >
-              >
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <svg-icon
                 type="mdi"
                 :path="path"
@@ -50,19 +46,18 @@
             <input
               id="allowWordClassificationModal"
               type="text"
-              class="block h-14 w-72 rounded-lg border border-gray-300
-              bg-gray-50 p-2.5 pl-10 text-lg text-gray-900 focus:border-blue-500 focus:ring-blue-500
-              dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400
-              dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              class="w-72 h-14 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500
+             focus:border-blue-500 block pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600
+             dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-lg"
               placeholder="NR Number Lookup"
               required
             >
           </div>
           <button
             type="submit"
-            class="ml-3 rounded-lg border border-blue-700 bg-blue-700 p-2.5 text-sm font-medium text-white
-            hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600
-            dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            class="p-2.5 ml-3 text-sm font-medium text-white bg-blue-700 rounded-lg border
+            border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300
+            dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <svg-icon
               type="mdi"
@@ -81,12 +76,12 @@
             class="peer sr-only"
           >
           <div
-            class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-[25px]
-            after:h-5 after:w-5after:rounded-full after:border after:border-gray-300
-          after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600
-            peer-checked:after:translate-x-full peer-checked:after:border-white
-            peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600
-          dark:bg-gray-700 dark:peer-focus:ring-blue-800"
+            class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300
+          dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full
+          peer-checked:after:border-white after:content-[''] after:absolute after:left-[2px] after:top-[25px]
+          after:bg-white after:border-gray-300
+          after:border after:rounded-full after:h-5 after:w-5 after:transition-all
+         dark:border-gray-600 peer-checked:bg-blue-600"
           />
           <span class="ml-3 font-medium text-gray-500 dark:text-gray-300">Classify Words</span>
         </label>
