67dd246021441754d90654f9e81f18bea048a5fa
Madhavkapoor28 <madhav.kapoor@gov.bc.ca>
2023-07-30 18:35:48 -0700

    priority queue option from navbar

--- components/StatsBox.vue
+++ components/StatsBox.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="stats-box mr-20 shadow-lg">
     <div class="mb-6 ml-2 mt-2">
-      Current status on {{ status.todayStr }}
+      Current status on
+      <span
+        id="date"
+      >{{ status.todayStr }}</span>
     </div>
     <div class="my-6 ml-2">
       Not Examined:
-      <span class="font-bold">{{ status.notExaminedNum }}</span>
+      <span
+        id="notExamined"
+        class="font-bold"
+      >{{ status.notExaminedNum }}</span>
     </div>
     <div class="mb-2 ml-2 mt-6">
-      Hold: <span class="font-bold">{{ status.holdNum }}</span>
+      Hold: <span
+        id="hold"
+        class="font-bold"
+      >{{ status.holdNum }}</span>
     </div>
   </div>
 </template>
 
 <script setup>
-import { Fetchstatus } from '~/store/fetchstatus'
+import { Fetchstatus } from '../store/fetchstatus'
 const status = Fetchstatus()
 
 status.getHoldedNum()
--- components/StdHeader.vue
+++ components/StdHeader.vue
@@ -47,7 +47,7 @@
             <input
               id="allowWordClassificationModal"
               type="text"
-              class="w-72 h-14 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500
+              class="w-72 h-13 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500
              focus:border-blue-500 block pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600
              dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-lg"
               placeholder="NR Number Lookup"
@@ -56,7 +56,7 @@
           </div>
           <button
             type="submit"
-            class="p-2.5 ml-3 text-sm font-medium text-white bg-blue-700 rounded-lg border
+            class="p-2.5 ml-3 h-12 text-white bg-blue-700 rounded-lg border
             border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300
             dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
@@ -67,7 +67,7 @@
           </button>
         </form>
 
-        <span class="mx-12 mt-7 underline">Stats</span>
+        <span class="mx-10 mt-7 underline">Stats</span>
 
         <label class="relative mt-2 inline-flex cursor-pointer items-center">
           <input
@@ -86,7 +86,24 @@
           <span class="ml-3 font-medium text-gray-500 dark:text-gray-300">Classify Words</span>
         </label>
 
-        <span class="mx-10 mt-7 flex text-gray-800">mkapoor@idir</span>
+        <label class="relative mt-2 inline-flex cursor-pointer items-center ml-5">
+          <input
+            type="checkbox"
+            value=""
+            class="peer sr-only"
+          >
+          <div
+            class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300
+          dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full
+          peer-checked:after:border-white after:content-[''] after:absolute after:left-[2px] after:top-[25px]
+          after:bg-white after:border-gray-300
+          after:border after:rounded-full after:h-5 after:w-5 after:transition-all
+         dark:border-gray-600 peer-checked:bg-blue-600"
+          />
+          <span class="ml-3 font-medium text-gray-500 dark:text-gray-300">Priority Queue</span>
+        </label>
+
+        <span class="mx-10 mt-7 flex text-gray-800">{{ KeycloakService.getUserInfo().fullName }}</span>
 
         <div class="divider" />
 
