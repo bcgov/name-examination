4666be729f9eecc76ba30334e5ba09a60542e762
Madhavkapoor28 <madhav.kapoor@gov.bc.ca>
2023-08-02 16:39:11 -0700

    checking-sso to initiate session once redirected

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   extends: [
     'google',
     'plugin:vue/vue3-essential',
-    // 'plugin:prettier/recommended',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/base',
@@ -27,16 +26,17 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'max-len': ['warn', { code: 120 }],
+    'max-len': ['warn', { code: 120, ignoreRegExpLiterals: true }],
     'vue/attribute-hyphenation': 'off',
-    'vue/multi-word-component-names': 'off',
+    'vue/no-deprecated-filter': 'warn',
+    'vue/no-deprecated-slot-scope-attribute': 'warn',
+    'vue/no-deprecated-v-on-native-modifier': 'warn',
     'vue/no-v-for-template-key-on-child': 'warn',
-    'vue/no-v-model-argument': 'warn',
+    'vue/no-v-html': 'off',
     'vue/v-on-event-hyphenation': 'off',
     '@typescript-eslint/ban-types': 'warn',
     '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-var-requires': 'off'
+    '@typescript-eslint/no-explicit-any': 'off'
   },
   overrides: [
     {
--- middleware/initKeycloak.global.ts
+++ middleware/initKeycloak.global.ts
@@ -1,10 +1,15 @@
 import KeycloakService from '~/public/keycloak/keycloak'
+import { isSigningIn } from '../util/utilities'
+import ConfigHelper from '../util/config-helper'
+import { SessionStorageKeys } from '../util/constants'
+import { useAuthStore } from '../store/auth'
 /**
  * This middleware function is called before the default layout is rendered.
  * It initializes Keycloak and syncs the session (eg, logged in/out).
  * NB: This does not run correctly as a plugin.
  */
 export default async function () {
+  const authModule = useAuthStore()
   console.log('Starting Keycloak service...')
 
   const keycloakConfig: any = {
@@ -14,4 +19,24 @@ export default async function () {
   }
 
   await KeycloakService.setKeycloakConfigUrl(keycloakConfig)
+
+  // Auto authenticate user once they are redirected from BC regisrty login page after enter credentials
+  if (isSigningIn()) {
+    // Initialize token service which will do a check-sso to initiate session
+    await KeycloakService.initializeToken(authModule,
+      true,
+      !authModule.isAuthenticated).then(() => {
+      const token = ConfigHelper.getFromSession(SessionStorageKeys.KeyCloakToken)
+      if (!token) {
+        // when there is no token in session storage, e.g. because token has expired due to user inactivity,
+        // make sure to clear out account info saved on previous login
+        ConfigHelper.removeFromSession(SessionStorageKeys.CurrentAccount)
+      }
+    }).catch((err) => {
+      if (err?.message !== 'NOT_AUTHENTICATED') {
+        throw err
+      }
+    })
+    await KeycloakService.initSession()
+  }
 }
