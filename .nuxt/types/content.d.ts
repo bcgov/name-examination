ac55555c2526969b443d654711a3b127135eea6b
Madhavkapoor28 <madhav.kapoor@gov.bc.ca>
2023-08-01 19:41:54 -0700

    Revert "tracking .nuxt as well now"
    This reverts commit 777ff73afa42e23de987f60a7f6fa35bd8f1ea0b.

--- .nuxt/types/content.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '#content/server' {
-  const serverQueryContent: typeof import('/home/madhavkapoor/repos/name-examination-upgrade/node_modules/@nuxt/content/dist/runtime/server').serverQueryContent
-  const parseContent: typeof import('/home/madhavkapoor/repos/name-examination-upgrade/node_modules/@nuxt/content/dist/runtime/server').parseContent
-}
--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = string
-declare module "/home/madhavkapoor/repos/name-examination-upgrade/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
