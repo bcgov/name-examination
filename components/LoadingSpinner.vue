2d8eaebec15dd4fc315ef9b9b27eb277e0d62c09
semmatti <suraj.emmattiparambil@gov.bc.ca>
2023-09-19 11:54:46 -0700

    create loading spinner component

---
+++ components/LoadingSpinner.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 transform"
+  >
+    <div
+      class="h-24 w-24 animate-spin rounded-full border-8 border-solid border-blue-400 border-t-transparent"
+    />
+  </div>
+</template>
--- components/SearchResultsBox.vue
+++ components/SearchResultsBox.vue
@@ -44,14 +44,7 @@
       </thead>
 
       <tbody v-if="filters.isLoading">
-        <!-- Spinner-->
-        <div
-          class="absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 transform"
-        >
-          <div
-            class="h-24 w-24 animate-spin rounded-full border-8 border-solid border-blue-400 border-t-transparent"
-          />
-        </div>
+        <LoadingSpinner />
       </tbody>
 
       <tbody v-else-if="filters.resultNum == 0">
