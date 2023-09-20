7c6ad638548409c6bd0b5cab5b1d3039361e98b1
semmatti <suraj.emmattiparambil@gov.bc.ca>
2023-09-20 11:26:01 -0700

    change `IconButton` color and make icon optional

--- components/IconButton.vue
+++ components/IconButton.vue
@@ -1,13 +1,19 @@
 <template>
   <button
-    class="w-20 rounded-md border bg-bcgov-blue5 px-4 py-0.5 text-white transition hover:bg-bcgov-gold5 hover:text-black"
+    class="inline-flex items-center justify-center w-20 rounded-md border bg-bcgov-blue5 px-4 py-0.5 text-white transition hover:bg-bcgov-blue8"
   >
-    <svg-icon type="mdi" viewBox="0 -3 24 24" :path="iconPath" />
+    <svg-icon
+      v-if="iconPath"
+      type="mdi"
+      viewBox="0 -3 24 24"
+      :path="iconPath"
+    />
+    <slot></slot>
   </button>
 </template>
 
 <script setup lang="ts">
 defineProps<{
-  iconPath: string
+  iconPath?: string
 }>()
 </script>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,10 @@ module.exports = {
     extend: {
       colors: {
         'bcgov-blue5': '#003366',
-        'bcgov-gold5': '#fcba19'
-      }
-    }
+        'bcgov-blue8': '#002049',
+        'bcgov-gold5': '#fcba19',
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 }
