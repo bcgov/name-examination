70c6c1d0704c19e3e6f2640c408ae4d551e525a4
semmatti <suraj.emmattiparambil@gov.bc.ca>
2023-10-05 14:59:18 -0700

    change loading spinner color

--- components/LoadingSpinner.vue
+++ components/LoadingSpinner.vue
@@ -3,7 +3,7 @@
     class="absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 transform"
   >
     <div
-      class="h-24 w-24 animate-spin rounded-full border-8 border-solid border-blue-400 border-t-transparent"
+      class="h-24 w-24 animate-spin rounded-full border-8 border-solid border-bcgov-blue5 border-t-transparent"
     />
   </div>
 </template>
