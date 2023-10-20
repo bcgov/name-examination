1522841b38b547d7163fa251380427319f123fc1
semmatti <suraj.emmattiparambil@gov.bc.ca>
2023-10-20 08:51:17 -0700

    start info header

---
+++ components/examine/request_info/Overview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col text-sm">
+    <div v-if="isPriority" class="flex items-center font-bold text-red-600">
+      <StarIcon class="h-4 w-4" />
+      <p>Priority</p>
+    </div>
+
+    <div>
+      <b>Status: </b> INPROGRESS
+    </div>
+
+    <div>
+      <b>Examiner: </b> anonymous@idir
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import { StarIcon } from '@heroicons/vue/24/solid'
+
+const isPriority = ref(true)
+</script>
--- components/examine/request_info/index.vue
+++ components/examine/request_info/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="h-32 bg-gray-100">
+  <div class="flex px-8 py-2 h-32 bg-gray-100">
+    <ExamineRequestInfoOverview />
     <ExamineRequestInfoCell />
   </div>
 </template>
