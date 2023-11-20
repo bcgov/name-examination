cbcbb77330c981512699e151598b8c4cd5983f64
semmatti <suraj.emmattiparambil@gov.bc.ca>
2023-11-20 11:38:10 -0800

    convert history info

--- components/examine/recipe/history/Info.vue
+++ components/examine/recipe/history/Info.vue
@@ -3,42 +3,61 @@
     <div class="flex basis-2/3 flex-col">
       <div class="grid grid-cols-2 gap-y-1 overflow-x-auto">
         <header class="font-bold">Client</header>
-        <p>Jane Frost</p>
+        <p>{{ applicants.clientFirstName }} {{ applicants.clientLastName }}</p>
 
         <header class="font-bold">Applicant</header>
         <div>
-          <p>Jack Frost</p>
-          <p>3434 Main Street</p>
-          <p>Suite 303</p>
-          <p>Top Floor</p>
-          <p>Saywarn BC v8v8v8 CA</p>
+          <p>{{ applicants.firstName }} {{ applicants.lastName }}</p>
+          <p>{{ applicants.addrLine1 }}</p>
+          <p>{{ applicants.addrLine2 }}</p>
+          <p>{{ applicants.addrLine3 }}</p>
+          <p>
+            {{ applicants.city }}
+            {{ applicants.stateProvinceCd }}
+            {{ applicants.postalCd }}
+            {{ applicants.countryTypeCd }}
+          </p>
         </div>
 
         <header class="font-bold">Phone</header>
-        <p>305-343-3434</p>
+        <p>{{ applicants.phoneNumber }}</p>
 
         <header class="font-bold">Email</header>
-        <p>anonymous@gov.bc.ca</p>
+        <p>{{ applicants.emailAddress }}</p>
 
         <header class="font-bold">Contact</header>
-        <p>jack frost</p>
+        <p>{{ applicants.contact }}</p>
       </div>
 
-      <header class="font-bold">Comments</header>
-      <ExamineCommentsBox
-        :comments="examine.comments"
-        class="max-h-48 overflow-auto rounded-md border border-gray-400 p-2 children:bg-sky-50"
-      />
+      <div v-if="conflict.comments && conflict.comments.length > 0">
+        <header class="font-bold">Comments</header>
+        <ExamineCommentsBox
+          :comments="conflict.comments"
+          class="max-h-48 overflow-auto rounded-md border border-gray-400 p-2 children:bg-sky-50"
+        />
+      </div>
     </div>
 
     <div class="grid basis-1/3 grid-cols-2 gap-y-1 overflow-x-auto">
-      <p class="font-bold">Name State</p>
-      <p>REJECTED</p>
+      <header class="font-bold">Name State</header>
+      <p>{{ conflict.state }}</p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useExamineStore } from '~/store/examine'
-const examine = useExamineStore()
+import type { NameRequestConflict } from '~/types'
+
+const { conflict } = defineProps<{
+  conflict: NameRequestConflict
+}>()
+
+const applicants = conflict.applicants
+
+const decisionText = computed(() => {
+})
+
+const conflicts = computed(() => {
+  
+})
 </script>
--- components/examine/recipe/history/index.vue
+++ components/examine/recipe/history/index.vue
@@ -5,14 +5,21 @@
     :rows="rows"
   >
     <template v-slot="{ index, row }">
-      <ExamineRecipeHistoryInfo />
+      <ExamineRecipeHistoryInfo
+        :conflict="examine.conflicts[1] as NameRequestConflict"
+      />
     </template>
   </ExamineRecipeTable>
 </template>
 
 <script setup lang="ts">
+import { useExamineStore } from '~/store/examine'
+import type { NameRequestConflict } from '~/types'
+
+const examine = useExamineStore()
+
 const rows = computed(() => [
   ['NOT SO STEEP COOPERATIVE', 'BC', 'NR 5979354', '2008-09-16', 'R'],
-  ['NOT SO STEEP COOPERATIVE', 'BC', 'NR 3252689', '2008-09-16', 'A'],
+  ['NOT SO STEEP COOPERATIVE', 'BC', 'NR 3252689', '2008-09-12', 'A'],
 ])
 </script>
