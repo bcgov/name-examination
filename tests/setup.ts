71d64729ec7405cc3d02419aef8dea59f04d0577
semmatti <suraj.emmattiparambil@gov.bc.ca>
2023-12-05 15:29:52 -0800

    change order of attributes in namex api mock

--- tests/setup.ts
+++ tests/setup.ts
@@ -4,6 +4,8 @@ beforeAll(() => {
   vi.mock('../util/namex-api', async (importOriginal) => {
     const mod = await importOriginal()
     return {
+      // @ts-ignore
+      ...mod,
       // Mock the url that is returned by getNamexApiUrl
       // so that it doesn't throw an error from creating an empty url when
       // the environment variables are undefined, such as when running in ci
@@ -11,8 +13,6 @@ beforeAll(() => {
         return new URL('/api/v1' + endpoint, 'https://localhost:8080/')
       }),
       callNamexApi: vi.fn(),
-      // @ts-ignore
-      ...mod,
     }
   })
 })
