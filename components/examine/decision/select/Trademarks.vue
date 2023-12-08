750e34addeef3fb48f66d752083efe6eac1beaa5
semmatti <suraj.emmattiparambil@gov.bc.ca>
2023-12-08 11:20:47 -0800

    differentiate trademark options in list select

--- components/examine/decision/select/Trademarks.vue
+++ components/examine/decision/select/Trademarks.vue
@@ -6,7 +6,7 @@
       multiple
       :options="examine.trademarksJSON.names"
       options-style="!max-h-40"
-      :options-display="(trademark: Trademark) => trademark.name"
+      :options-display="displayTrademark"
       :disabled="examine.decisionSelectionsDisabled"
     >
       <Chips
@@ -23,4 +23,25 @@ import { useExamineStore } from '~/store/examine'
 import type { Trademark } from '~/types'
 
 const examine = useExamineStore()
+
+const trademarkCounts = computed(() => {
+  const counts: { [trademarkName: string]: number } = {}
+  for (const trademark of examine.trademarksJSON.names) {
+    if (trademark.name in counts) {
+      counts[trademark.name] += 1
+    } else {
+      counts[trademark.name] = 1
+    }
+  }
+  return counts
+})
+
+/**
+ * Get the display string for a trademark object.
+ * If there are multiple trademarks with the same name, append the application number.
+ */
+const displayTrademark = (trademark: Trademark) =>
+  trademarkCounts.value[trademark.name] > 1
+    ? `${trademark.name} (#${trademark.application_number})`
+    : trademark.name
 </script>
