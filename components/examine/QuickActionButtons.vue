ae1cc80b450a8cb697bf6db023e61ec49e40c75d
semmatti <suraj.emmattiparambil@gov.bc.ca>
2023-12-18 10:21:33 -0800

    remove mnemonic display from quick approve

--- components/examine/QuickActionButtons.vue
+++ components/examine/QuickActionButtons.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="flex h-9 space-x-1">
-    <!-- A mnemonic is displayed but not implemented for this button since the Approve Name button in the 
-    decision panel also has a mnemonic of 'a' and they will always appear on the screen at the same time,
-    so only one of them needs to have a mnemonic attribute so the action doesn't trigger twice-->
     <IconButton white class="border-none" @click="quickApprove">
       <CheckCircleIcon class="h-7 w-7 text-lime-600" />
-      <template #text>Quick&nbsp;<u>A</u>pprove</template>
+      <template #text>Quick Approve</template>
     </IconButton>
 
     <IconButton
