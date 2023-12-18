8501b860607b19bce99e4d705b077a84468343d8
semmatti <suraj.emmattiparambil@gov.bc.ca>
2023-12-18 09:01:54 -0800

    fix search input mnemonic

--- components/SearchInput.vue
+++ components/SearchInput.vue
@@ -1,9 +1,8 @@
 <template>
   <form class="flex items-center" role="search">
-    <label :for="TEXT_INPUT_ID" class="sr-only">{{ placeholder }}</label>
     <div class="w-full">
       <input
-        :id="TEXT_INPUT_ID"
+        ref="searchTextField"
         type="search"
         class="w-full rounded-l-md border border-gray-300 px-2 py-1 text-gray-900 focus:border-blue-500 focus:outline-none focus:ring-blue-500"
         :placeholder="placeholder"
@@ -11,6 +10,7 @@
         @change="(e) => $emit('update:modelValue', (e.target as HTMLInputElement).value)"
         autocorrect="off"
         required
+        aria-label="Search"
       />
     </div>
     <IconButton
@@ -30,11 +30,9 @@ const { focusMnemonic } = defineProps<{
   focusMnemonic?: string
 }>()
 
-const TEXT_INPUT_ID = 'searchInputTextField'
+const searchTextField = ref<HTMLInputElement | null>(null)
 
 if (focusMnemonic) {
-  useMnemonic(focusMnemonic, () =>
-    document.getElementById(TEXT_INPUT_ID)?.focus()
-  )
+  useMnemonic(focusMnemonic, () => searchTextField.value?.focus())
 }
 </script>
