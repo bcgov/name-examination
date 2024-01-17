094d61ee8c2052f76bf19799b1f40fcd2f991ccb
semmatti <suraj.emmattiparambil@gov.bc.ca>
2024-01-17 12:07:32 -0800

    disable button when save edit is loading

--- components/examine/header/EditActionButtons.vue
+++ components/examine/header/EditActionButtons.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="flex h-fit items-center space-x-1">
-    <IconButton
-      :disabled="savingEdit"
-      :class="{ 'opacity-60 hover:opacity-60': savingEdit }"
-      light
-      mnemonic="s"
-      @click="save"
-    >
+    <IconButton :disabled="savingEdit" light mnemonic="s" @click="save">
       <CheckIcon v-if="!savingEdit" class="h-5 w-5 stroke-2" />
       <ArrowPathIcon v-else class="mr-0.5 h-5 w-5 animate-spin stroke-2" />
       <template #text><u>S</u>ave Edits</template>
     </IconButton>
 
-    <IconButton light mnemonic="c" @click="examine.cancelEdits">
+    <IconButton
+      :disabled="savingEdit"
+      light
+      mnemonic="c"
+      @click="examine.cancelEdits"
+    >
       <XMarkIcon class="h-5 w-5 stroke-2" />
       <template #text><u>C</u>ancel</template>
     </IconButton>
