4df097c1a734d3f1841159aa8d2e41ad35ec0263
semmatti <suraj.emmattiparambil@gov.bc.ca>
2024-01-24 15:26:44 -0800

    preserve scroll between recipe tab changes

--- components/examine/recipe/tabs/index.vue
+++ components/examine/recipe/tabs/index.vue
@@ -1,33 +1,34 @@
 <template>
   <div class="sm:px-0">
-    <TabGroup>
+    <TabGroup @change="onChangeTab">
       <ExamineRecipeTabsHeader />
 
       <TabPanels
+        ref="panels"
         class="mt-2 h-[29rem] overflow-auto rounded-md border border-gray-300 bg-white"
       >
         <!-- Conflicts tab -->
-        <TabPanel :unmount="false">
+        <TabPanel ref="conflicts" :unmount="false">
           <ExamineRecipeConflicts />
         </TabPanel>
 
         <!-- Conditions tab -->
-        <TabPanel :unmount="false">
+        <TabPanel ref="conditions" :unmount="false">
           <ExamineRecipeConditions />
         </TabPanel>
 
         <!-- Trademarks tab -->
-        <TabPanel :unmount="false">
+        <TabPanel ref="trademarks" :unmount="false">
           <ExamineRecipeTrademarks />
         </TabPanel>
 
         <!-- History tab -->
-        <TabPanel :unmount="false">
+        <TabPanel ref="history" :unmount="false">
           <ExamineRecipeHistory />
         </TabPanel>
 
         <!-- Compare tab -->
-        <TabPanel :unmount="false">
+        <TabPanel ref="compare" :unmount="false">
           <ExamineRecipeCompare />
         </TabPanel>
       </TabPanels>
@@ -37,4 +38,37 @@
 
 <script setup lang="ts">
 import { TabGroup, TabPanels, TabPanel } from '@headlessui/vue'
+
+const conflicts = ref<InstanceType<typeof TabPanel>>()
+const conditions = ref<InstanceType<typeof TabPanel>>()
+const trademarks = ref<InstanceType<typeof TabPanel>>()
+const history = ref<InstanceType<typeof TabPanel>>()
+const compare = ref<InstanceType<typeof TabPanel>>()
+const panels = ref<InstanceType<typeof TabPanels>>()
+
+const tabs = [conflicts, conditions, trademarks, history, compare]
+const lastIndex = ref(0)
+
+const scrollPositions: Array<number> = new Array(5).fill(0)
+
+type TabElementRef =
+  | Ref<InstanceType<typeof TabPanel> | undefined>
+  | Ref<InstanceType<typeof TabPanels> | undefined>
+
+function getTop(elem: TabElementRef) {
+  return (elem.value?.$el.getBoundingClientRect().top as number) ?? 0
+}
+
+function setTop(elem: TabElementRef, top: number) {
+  setTimeout(() => {
+    elem.value?.$el.scrollTo({ top, behavior: 'instant' })
+  }, 1)
+}
+
+function onChangeTab(index: number) {
+  setTop(panels, scrollPositions[index])
+  const panelTop = getTop(panels)
+  scrollPositions[lastIndex.value] = panelTop - getTop(tabs[lastIndex.value])
+  lastIndex.value = index
+}
 </script>
