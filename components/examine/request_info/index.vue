7e7f2fc9777e0660ba323cc48581279b4e299eb1
semmatti <suraj.emmattiparambil@gov.bc.ca>
2024-01-30 15:27:10 -0800

    use width instead of basis for request info cells

--- components/examine/request_info/index.vue
+++ components/examine/request_info/index.vue
@@ -3,11 +3,11 @@
     class="flex divide-x-2 bg-gray-100 px-4 py-2"
     :class="isExpanded ? 'h-full' : 'h-32'"
   >
-    <ExamineRequestInfoStatus class="basis-1/5 px-4" />
-    <ExamineRequestInfoDatesAndExpiry class="basis-1/5 px-4" />
-    <ExamineRequestInfoAdditionalInfo class="basis-1/5 px-4" />
-    <ExamineRequestInfoNatureOfBusiness class="basis-1/5 px-4" />
-    <ExamineRequestInfoApplicant class="basis-1/5 px-4" />
+    <ExamineRequestInfoStatus class="w-1/5 px-4" />
+    <ExamineRequestInfoDatesAndExpiry class="w-1/5 px-4" />
+    <ExamineRequestInfoAdditionalInfo class="w-1/5 px-4" />
+    <ExamineRequestInfoNatureOfBusiness class="w-1/5 px-4" />
+    <ExamineRequestInfoApplicant class="w-1/5 px-4" />
   </div>
 </template>
 
