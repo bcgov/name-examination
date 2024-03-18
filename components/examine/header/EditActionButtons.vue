53e41ad26164a9c445978edc524f6f5ba54da256
semmatti <suraj.emmattiparambil@gov.bc.ca>
2024-03-18 14:32:54 -0700

    increase edit action button spacing

--- components/examine/header/EditActionButtons.vue
+++ components/examine/header/EditActionButtons.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-fit items-center space-x-1">
+  <div class="flex h-fit items-center space-x-3">
     <IconButton :disabled="savingEdit" light mnemonic="s" @click="save">
       <CheckIcon v-if="!savingEdit" class="h-5 w-5 stroke-2" />
       <ArrowPathIcon v-else class="mr-0.5 h-5 w-5 animate-spin stroke-2" />
