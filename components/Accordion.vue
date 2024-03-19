1a05413281da55e848307a224ac5c8f7db524344
semmatti <suraj.emmattiparambil@gov.bc.ca>
2024-03-19 09:38:33 -0700

    simplify `Accordion`

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -1,5 +1,9 @@
 <template>
-  <details :class="{ 'pointer-events-none': disabled }" ref="details">
+  <details
+    :class="{ 'pointer-events-none': disabled }"
+    ref="details"
+    @toggle="(e: ToggleEvent) => open = e.newState === 'open'"
+  >
     <summary
       class="flex w-full cursor-pointer items-center justify-between rounded p-1 text-left text-sm font-medium transition"
       :class="buttonStyle"
@@ -11,8 +15,8 @@
         class="ml-1 h-5 w-5 stroke-2 transition"
       />
     </summary>
-    <div v-if="open" class="p-1">
-      <slot name="content"></slot>
+    <div class="p-1">
+      <slot v-if="open" name="content"></slot>
     </div>
   </details>
 </template>
@@ -21,23 +25,11 @@
 import { ChevronDownIcon } from '@heroicons/vue/24/outline'
 
 const details = ref<HTMLDetailsElement>()
-const open = ref(false)
-
-onMounted(() => {
-  open.value = Boolean(details.value?.open)
-  details.value?.addEventListener('toggle', () => {
-    open.value = Boolean(details.value?.open)
-    emit('toggled', open.value)
-  })
-})
+const open = ref(Boolean(details.value?.open))
 
 defineProps<{
   arrow?: boolean
   buttonStyle?: any
   disabled?: boolean
-}>()
-
-const emit = defineEmits<{
-  toggled: [isOpen: boolean]
 }>()
 </script>
