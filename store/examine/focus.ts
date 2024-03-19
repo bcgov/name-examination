814536d671c17a63d7d14fb00eff376ed3f30b09
semmatti <suraj.emmattiparambil@gov.bc.ca>
2024-03-19 15:01:44 -0700

    update focus to handle focus changes outside tab key

--- store/examine/focus.ts
+++ store/examine/focus.ts
@@ -4,36 +4,39 @@ import { useExamination } from '.'
 /** Handles the focusing of elements in the Examine page */
 export const useExaminationFocus = defineStore('examine-focus', () => {
   /** Array of element ids */
-  const elements = reactive<{ [key: number]: string }>({})
-  const elementCount = computed(() => Object.keys(elements).length)
+  const elements = ref<Array<string>>([])
   const focused = ref(-1)
   const examine = useExamination()
 
-  function getCurrentRoute() {
-    return useRoute().path.toString().toLowerCase()
-  }
-
   /** Register an element to be a part of the focus cycle. */
   function register(index: number, elementId: string) {
-    if (elements[index] && elements[index] !== elementId) {
+    if (index > elements.value.length - 1) {
+      elements.value.length = index + 1
+    }
+    if (elements.value[index] && elements.value[index] !== elementId) {
       console.warn(
-        `Overwriting previously registered focus item '${elements[index]}' with '${elementId}' at index ${index}`
+        `Overwriting previously registered focus item '${elements.value[index]}' with '${elementId}' at index ${index}`
       )
     }
-    elements[index] = elementId
+    elements.value[index] = elementId
   }
 
   /** Handle an incoming `KeyboardEvent`.
    * Will focus the proper element in the focus cycle when the `Tab` key is pressed.
    */
   function handleKeyPress(event: KeyboardEvent) {
-    const route = getCurrentRoute()
+    const route = useRoute().path.toString().toLowerCase()
     if (route !== Route.Examine || event.code !== 'Tab' || examine.isEditing)
       return
-
+    // handle tab key
     if (event.type === 'keyup') {
-      focused.value = (focused.value + 1) % elementCount.value
-      document.getElementById(elements[focused.value])?.focus()
+      // update the focused element if it was changed through some means other than the tab key
+      const activeElemIndex = elements.value.indexOf(document.activeElement?.id || '')
+      if (activeElemIndex !== -1) {
+        focused.value = activeElemIndex
+      }
+      focused.value = (focused.value + 1) % elements.value.length
+      document.getElementById(elements.value[focused.value])?.focus()
     } else if (event.type === 'keydown') {
       event.preventDefault()
     }
