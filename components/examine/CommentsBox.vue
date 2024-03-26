199fd7c1dc77849943d57a734ea2c4404f5726cf
eve-git <54647458+eve-git@users.noreply.github.com>
2024-03-26 10:13:48 -0700

    Update components/examine/CommentsBox.vue
    Co-authored-by: semmatti <144159881+semmatti@users.noreply.github.com>

--- components/examine/CommentsBox.vue
+++ components/examine/CommentsBox.vue
@@ -5,7 +5,7 @@
       class="comments-box-child flex flex-col rounded border border-gray-400 bg-white p-2 text-sm"
       v-for="comment in comments"
     >
-      <p v-html="comment.html" />
+      <p class="whitespace-pre-wrap">{{ comment.comment }}</p>
       <span class="font-bold">
         {{ comment.examiner }}
       </span>
