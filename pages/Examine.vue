ff6d1d92b93b4bbccb264d44e3bb8e6d7187d60d
rarmitag <35737789+rarmitag@users.noreply.github.com>
2024-04-11 14:45:44 -0700

    Update pages/Examine.vue
    Co-authored-by: semmatti <144159881+semmatti@users.noreply.github.com>

--- pages/Examine.vue
+++ pages/Examine.vue
@@ -52,8 +52,8 @@
       "
       class="flex grow flex-col space-x-3 bg-gray-100 p-4 2xl:flex-row"
     >
-      <ExamineRecipe class="xl:w-1/2" />
-      <ExamineDecision v-if="showDecisionPanel" class="xl:w-1/2" />
+      <ExamineRecipe class="2xl:w-1/2" />
+      <ExamineDecision v-if="showDecisionPanel" class="2xl:w-1/2" />
     </div>
   </div>
 </template>
