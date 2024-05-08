57464e905dde39c3290f932242d1f4fc11265505
rstens <roland.stens.com>
2024-05-07 17:50:17 -0700

    feat: Add Cypress testing to the project

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,9 @@ export default defineVitestConfig({
   test: {
     environment: 'nuxt',
     setupFiles: ['./tests/vue-test-utils-plugins.ts', './tests/setup.ts'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './tests/unit/coverage',
+    },
   },
 })
