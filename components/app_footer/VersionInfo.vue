b36d778636c7aa9d6ab9a3786c35085e4a4c6979
Your Other Username <Ethan.Portman@gov.bc.ca>
2024-05-11 15:27:37 -0700

    Added VersionInfo component to be used by Footer, does not grab correct versions yet

---
+++ components/app_footer/VersionInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="relative inline-block">
+    <button
+      class="text-blue-500 pl-4"
+      @mouseover="showVersionNumbers = true"
+      @mouseleave="showVersionNumbers = false"
+    >
+      Version
+    </button>
+    <div
+      v-if="showVersionNumbers"
+      class="absolute z-10 w-40 left-0 transform -translate-x-full top-[-3.5rem]
+              px-5 py-5 bg-gray-800 text-white text-xs rounded-lg shadow-lg"
+    >
+      <div>Name Examination: {{ nameExaminationVersion }}</div>
+      <div>NameX: {{ nameXVersion }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const showVersionNumbers = ref(false)
+const nameExaminationVersion = 'TO_DO'
+const nameXVersion = ref('TO_DO')
+</script>
