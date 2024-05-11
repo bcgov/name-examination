0c156be7b871b506b072dc93afdbd5a9e562d3be
Your Other Username <Ethan.Portman@gov.bc.ca>
2024-05-11 15:57:17 -0700

    Added method to fetch NameX Version. Endpoint does not work, needs fixing

--- components/app_footer/VersionInfo.vue
+++ components/app_footer/VersionInfo.vue
@@ -20,9 +20,34 @@
 
 <script setup lang="ts">
 import packageInfo from '~/package.json'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const showVersionNumbers = ref(false)
 const nameExaminationVersion = packageInfo.version
-const nameXVersion = ref('TO_DO')
+const nameXVersion = ref('')
+
+onMounted(async () => {
+  nameXVersion.value = await fetchNameXVersion()
+})
+
+/* THESE ENDPOINTS ARE NOT WORKING. CORS ISSUE? */
+const fetchNameXVersion = async (): Promise<string> => {
+  const devNameXVersionEndpoint = 'https://namex-dev.apps.silver.devops.gov.bc.ca/api/v1/meta/info'
+  // const testNameXVersionEndpoint = 'https://namex-test.apps.silver.devops.gov.bc.ca/api/v1/meta/info'
+  // const prodNameXVersionEndpoint = 'https://namex.apps.silver.devops.gov.bc.ca/api/v1/meta/info'
+
+  try {
+    const response = await fetch(devNameXVersionEndpoint)
+    if (!response.ok) {
+      console.error('Failed to fetch:', response.status, response.statusText)
+      throw Error
+    }
+    const responseJson = await response.json()
+    return responseJson.API
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    return 'Error'
+  }
+}
+
 </script>
