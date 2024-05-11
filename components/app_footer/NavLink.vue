c5d8528f96bb63b918c2ee8eed2b9cd1a7a349d0
Your Other Username <Ethan.Portman@gov.bc.ca>
2024-05-11 15:25:20 -0700

    Added a reusable link to be used in the app footer

---
+++ components/app_footer/NavLink.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="inline-block mr-1 xl:mr-2 lg:border-r lg:pr-2 border-solid border-bcgov-blue3">
+    <nuxt-link
+      :to="route"
+      class="block py-1 px-2 text-white no-underline hover:underline xl:px-[0.25rem] xl:px-[0.5rem]"
+      :target="target"
+    >
+      {{ text }}
+    </nuxt-link>
+  </li>
+</template>
+
+<script setup lang="ts">
+/** A navigation link that takes you to another page. */
+const { route } = defineProps<{
+  /** The text that the link should display */
+  text: string
+  /** The page this link should lead to */
+  route: string
+  /** Where to open the linked page  */
+  target?: string
+}>()
+</script>
