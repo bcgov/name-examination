b31aa40eb147119990cb140aad7e4f98a6764654
EPortman <Ethan.Portman@gov.bc.ca>
2024-07-22 13:24:19 -0700

    Made getting value from local storage more versatile

--- app/util/index.ts
+++ app/util/index.ts
@@ -149,9 +149,16 @@ export const getLocalStorageValue = <T>(key: string, defaultValue: T): T => {
     return defaultValue
   }
   try {
-    return JSON.parse(storedValue) as T
+    // Try to parse as JSON first
+    const item = JSON.parse(storedValue) as T
+    return item
   } catch (error) {
-    window.localStorage.setItem(key, JSON.stringify(defaultValue))
-    return defaultValue
+    // If parsing fails and the type is string, return the stored value directly
+    if (typeof defaultValue === 'string') {
+      return storedValue as unknown as T
+    } else {
+      window.localStorage.setItem(key, JSON.stringify(defaultValue))
+      return defaultValue
+    }
   }
 }
