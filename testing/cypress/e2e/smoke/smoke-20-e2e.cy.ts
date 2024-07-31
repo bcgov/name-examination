8d5b22b6f24fea60eac8601a1824ed16b9d8ba55
EPortman <Ethan.Portman@gov.bc.ca>
2024-07-31 12:32:05 -0700

    Got it working, try with others

--- testing/cypress/e2e/smoke/smoke-20-e2e.cy.ts
+++ testing/cypress/e2e/smoke/smoke-20-e2e.cy.ts
@@ -28,20 +28,6 @@ describe('E2E Smoke Test', () => {
     const nrNum = '3351228'
     homePage.examineNamesLink()
   
-    cy.get(homePage.searchInputField).should('be.visible')
-      .then(($input) => {
-        cy.wrap($input).type(nrNum, { force: true })}
-      )
-    
-    cy.waitForSpinner()
-    cy.get(homePage.searchButton).should('be.visible').click({ force: true })
-    cy.contains(homePage.nrNumberHeader, nrNum).should('exist')
-  })
-
-  it('Should be able to examine an NR2', () => {
-    const nrNum = '3351228'
-    homePage.examineNamesLink()
-  
     cy.get(homePage.searchInputField).should('exist')
       .then(($input) => {
         cy.wrap($input).type(nrNum, { force: true })}
@@ -52,21 +38,7 @@ describe('E2E Smoke Test', () => {
     cy.wait('@getRequest')
     cy.contains(homePage.nrNumberHeader, nrNum).should('exist')
   })
-
-  it('Should be able to examine an NR3', () => {
-    const nrNum = '3351228'
-    homePage.examineNamesLink()
   
-    cy.get(homePage.searchInputField).should('exist')
-      .then(($input) => {
-        cy.wrap($input).type(nrNum, { force: true })}
-      )
-    
-    cy.waitForSpinner()
-    cy.get(homePage.searchButton).should('exist').click({ force: true })
-    cy.wait(3000)
-    cy.contains(homePage.nrNumberHeader, nrNum).should('exist')
-  })
 
   it('Should be able to search an NR', () => {
     const nrNum = '3351228'
