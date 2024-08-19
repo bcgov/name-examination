ddcea58ea57e483d53c324945725f34563028c93
EPortman <Ethan.Portman@gov.bc.ca>
2024-08-19 11:07:49 -0700

    feat: add test for conditional approval

--- testing/cypress/e2e/smoke/smoke-20-approvals.cy.ts
+++ testing/cypress/e2e/smoke/smoke-20-approvals.cy.ts
@@ -1,9 +1,10 @@
 // Cypress Test: Check for ability to approve NRs.
 // This spec checks that NRs can be approved with the approval button and quick approval button.
+// This spec also checks that NRs can be conditionally approved.
 import HomePage from 'cypress/pageObjects/homePage'
 import SearchPage from 'cypress/pageObjects/searchPage'
 import ExaminePage from 'cypress/pageObjects/examinePage'
-import { NRState } from 'cypress/constants'
+import { NRState, DecisionText } from 'cypress/constants'
 const homePage = new HomePage()
 const searchPage = new SearchPage()
 const examinePage = new ExaminePage()
@@ -56,6 +57,30 @@ describe('Check that approving NRs works', () => {
     cy.get(examinePage.choice1CheckMark).should('be.visible')
     cy.get('@nrNum').then((nrNum) => {
       cy.verifyNRState(nrNum.toString(), NRState.Approved)
+    })
+  })
+
+
+  it('Should be able to conditionally approve a NR', () => {
+    // Get the first NR from the Examine Names button.
+    homePage.examineNamesLink()
+    examinePage.retrieveCurrentNr()
+    cy.get(examinePage.nrStatus).should('have.text', NRState.InProgress)
+
+    // Activate the consent required checkbox and then approve the name.
+    examinePage.toggleConsentCheckbox(true)
+    examinePage.clickApproveButton()
+
+    // Verify conditional approval on examine page.
+    cy.get(examinePage.nrStatus).should('have.text', NRState.Conditional)
+    cy.get(examinePage.choice1DecisionTxt)
+      .invoke('text')
+      .then((text) => text.trim())
+      .should('eq', DecisionText.ApproveConditionalConsent)
+
+    // Verify conditional approval on search page.
+    cy.get('@nrNum').then((nrNum) => {
+      cy.verifyNRState(nrNum.toString(), NRState.Conditional)
     })
   })
 })
