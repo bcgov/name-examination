10d17160805a938070412b92eaad6e849c9605de
eve-git <eve.deng@gov.bc.ca>
2025-01-22 10:09:34 -0800

    the error page

---
+++ app/pages/error.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-center mt-20">
+      <h1 class="text-2xl font-bold mb-4">Access Denied</h1>
+      <p class="text-lg">You are not authorized to access this application.</p>
+    </div>
+  </template>
+
+<script setup>
+definePageMeta({
+  layout: 'empty', // Use the custom blank layout
+});
+</script>
