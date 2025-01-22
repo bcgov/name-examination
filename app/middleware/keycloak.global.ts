9031803f2e0e7d89a9d99a70d220dce04a8c4764
eve-git <eve.deng@gov.bc.ca>
2025-01-22 10:11:08 -0800

    revert back the comment

--- app/middleware/keycloak.global.ts
+++ app/middleware/keycloak.global.ts
@@ -7,6 +7,7 @@ export default defineNuxtRouteMiddleware(async (to) => {
   }
   
   // remove query params in url added by keycloak
+  // from https://github.com/bcgov/business-transparency-registry/blob/3a8a8364c3279859e130f2c7aae4feee6b65c5a2/btr-web/btr-common-components/middleware/setupAuth.global.ts#L13
   if (to.query) {
     const params = new URLSearchParams(to.fullPath.split('?')[1])
     params.delete('state')
