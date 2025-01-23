54f169c410d10530cb381bc2dbe019e208309aca
eve-git <54647458+eve-git@users.noreply.github.com>
2025-01-23 08:42:44 -0800

    #24487 Access to the application is restricted to staff members only (#1553)
    * only staff can access app
    
    * the error page
    
    * revert back the comment
    
    * update

--- app/middleware/keycloak.global.ts
+++ app/middleware/keycloak.global.ts
@@ -1,4 +1,11 @@
 export default defineNuxtRouteMiddleware(async (to) => {
+
+  // Check metadata to skip middleware
+  if (to.meta.layout === 'empty') {
+    console.log('Skipping middleware for routes with blank layout.');
+    return;
+  }
+  
   // remove query params in url added by keycloak
   // from https://github.com/bcgov/business-transparency-registry/blob/3a8a8364c3279859e130f2c7aae4feee6b65c5a2/btr-web/btr-common-components/middleware/setupAuth.global.ts#L13
   if (to.query) {
@@ -8,5 +15,22 @@ export default defineNuxtRouteMiddleware(async (to) => {
     params.delete('code')
     params.delete('error')
     to.fullPath = to.path + (params.size > 0 ? `?${params}` : '') + to.hash
+  }
+
+  // Access the Keycloak instance
+  const { $auth } = useNuxtApp(); 
+  console.log('Auth object:', JSON.stringify($auth, null, 2));
+
+  // Check if authentication data is available
+  if (!$auth?.tokenParsed ) {
+    console.error('Authentication object is not available. Redirecting to error page.');
+    return navigateTo('/error');
+  }
+ 
+  // Check if the user is a staff member
+  const userRoles = $auth.tokenParsed?.realm_access?.roles;
+  if (!userRoles?.includes('staff')) {
+    console.error('Access denied: You are not authorized to use this application.');
+    return navigateTo('/error'); // Redirect to an error page or login page
   }
 })
